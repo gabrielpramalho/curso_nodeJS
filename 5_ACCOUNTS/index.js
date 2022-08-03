9ad64a2403f7c2213ab1b113b963d07d190714ec
GABRIEL PEDRO RAMALHO <biel-pedro@hotmail.com>
2022-08-03 11:08:41 -0300

    accounts

--- 5_ACCOUNTS/index.js
+++ 5_ACCOUNTS/index.js
@@ -1,8 +1,33 @@
 const inquirer = require('inquirer');
 const chalk = require("chalk")
 
-const fs = require("fs")
+const fs = require("fs");
+const Choices = require('inquirer/lib/objects/choices');
+
+operation()
 
 function operation(){
-    
+
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'action',
+            message: 'O que você deseja fazer?',
+            choices: [
+                'Criar Conta',
+                'Consutar Saldo',
+                'Depositar',
+                'Sacar',
+                'Sair'
+            ]
+        },
+    ]).then((answer) => {
+        
+
+        const action = answer['action']
+        console.log(action);
+
+    })
+    .catch((err) => console.log(err))
+
 }
